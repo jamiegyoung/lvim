0cc35a539d908177c972cedf09b33249e3225ea9
jamiegyoung <jamie@fullfatthings.com>
2023-10-13 09:41:36 +0100

    Add hop.vim

--- config.lua
+++ config.lua
@@ -16,12 +16,38 @@ lvim.plugins = {
       require("copilot_cmp").setup()
     end,
   },
+  {
+    'smoka7/hop.nvim',
+    version = '*', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end
+  }
 }
 
 lvim.colorscheme = "catppuccin-frappe"
 
+-- Hop config
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+vim.keymap.set('', 'f', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+end, {remap=true})
+vim.keymap.set('', 'F', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+end, {remap=true})
+vim.keymap.set('', 't', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })
+end, {remap=true})
+vim.keymap.set('', 'T', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })
+end, {remap=true})
+
+-- Set relative line numbers
 vim.opt.relativenumber = true
 
+-- Remove trailing whitespace on save
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
   command = [[%s/\s\+$//e]],
@@ -51,4 +77,3 @@ copilot.setup {
 
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
-
