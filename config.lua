3ef9bdfec1e98b4d15bed5c60f3931d49f268579
jamiegyoung <jamie@fullfatthings.com>
2023-12-04 13:54:02 +0000

    feat: remove hop

--- config.lua
+++ config.lua
@@ -16,14 +16,6 @@ lvim.plugins = {
       require("copilot_cmp").setup()
     end,
   },
-  {
-    'smoka7/hop.nvim',
-    version = '*', -- optional but strongly recommended
-    config = function()
-      -- you can configure Hop the way you like here; see :h hop-config
-      require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-    end
-  }
 }
 
 lvim.colorscheme = "catppuccin-frappe"
@@ -38,22 +30,6 @@ formatters.setup {
     extra_args = { "--line-length", "79" },
   }
 }
-
--- Hop config
-local hop = require('hop')
-local directions = require('hop.hint').HintDirection
-vim.keymap.set('', 'f', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
-end, {remap=true})
-vim.keymap.set('', 'F', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
-end, {remap=true})
-vim.keymap.set('', 't', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })
-end, {remap=true})
-vim.keymap.set('', 'T', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })
-end, {remap=true})
 
 -- Set relative line numbers
 vim.opt.relativenumber = true
