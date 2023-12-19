7a0860f5564340897e9258370e5dec37a0d8679d
jamiegyoung <jamie@fullfatthings.com>
2023-12-19 14:51:00 +0000

    Add markdown preview

--- config.lua
+++ config.lua
@@ -32,6 +32,12 @@ lvim.plugins = {
       require("copilot_cmp").setup()
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  }
 }
 
 lvim.colorscheme = "catppuccin-frappe"
